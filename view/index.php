00bda9449a859cdd23b33ea8ef1adf46a3382ad5
Mirna Imrović <imrovmirna@gmail.com>
2022-06-16 22:45:28 +0200

    sitni popravak html-a

--- view/index.php
+++ view/index.php
@@ -2,7 +2,7 @@
 
 require_once __SITE_PATH . '/view/_header.php'; 
 
-echo "<h3>Welcome to the Family Trees app! </h4>";
+echo "<h3>Welcome to the Family Trees app! </h3>";
 
 require_once __SITE_PATH . '/view/_footer.php'; 
 
